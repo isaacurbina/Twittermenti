61ac4bf0b1653a0cafe7a60a15da60b8509c3839
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-01-15 18:03:13 -0600

    355. Setting Up the Swifter Framework (#3)

--- Twittermenti/ViewController.swift
+++ Twittermenti/ViewController.swift
@@ -7,22 +7,37 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
-    
-    @IBOutlet weak var backgroundView: UIView!
-    @IBOutlet weak var textField: UITextField!
-    @IBOutlet weak var sentimentLabel: UILabel!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-
-    @IBAction func predictPressed(_ sender: Any) {
-    
-    
-    }
-    
+	
+	// MARK: - IBOutlets
+	
+	@IBOutlet weak var backgroundView: UIView!
+	@IBOutlet weak var textField: UITextField!
+	@IBOutlet weak var sentimentLabel: UILabel!
+	
+	// MARK: - constants/objects
+	
+	private var swifter: Swifter = Swifter(
+		consumerKey: "LTReJyreyyuz4SWjVABuA1WKI",
+		consumerSecret: "ajf107xfd6869heCG8Ptn8iztFHBWPlxLkFGvsmoEwI7MKZcz"
+	)
+	
+	// MARK: - UIViewController
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		swifter.searchTweet(using: "@Apple", count: 10, success: { (results, metadata) in
+			print(results)
+		}) { (error) in
+			print("There was an error with the Twitter API Request, \(error)")
+		}
+	}
+	
+	// MARK: - IBActions
+	
+	@IBAction func predictPressed(_ sender: Any) {
+		
+	}
 }
-
